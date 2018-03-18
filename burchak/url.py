328682a76ed468dc7b3438acf23d8079826d1044
Павел <paul114burchak@gmail.com>
2018-03-18 15:34:29 +0200

    parse xml

--- burchak/url.py
+++ burchak/url.py
@@ -1,18 +1,26 @@
 from lxml import html
 import requests
 import re
+import xml.etree.ElementTree as ET
+
 
 def get_urls(url, i):
     page = requests.get(url)
     webpage = html.fromstring(page.content)
     for link in webpage.xpath('//a/@href'):
         if i > 1:
-            if re.match(r'https?:\/\/.*',link):
+            if re.match(r"https?:\/\/.*", link):
                 get_urls(link, i-1)
             else:
-                get_urls(url[:len(url)-1]+link,i-1)
+                get_urls(url[:len(url)-1]+link, i-1)
         else:
-            print(link,"url",url,"\n")
-    return;
+            print(link, "url", url, "\n")
+    return
 
-get_urls('http://gdk-kpi.herokuapp.com/',2)
+
+tree = ET.parse('ulr.xml')
+root = tree.getroot()
+depth = int(root.find('depth').text)
+print(depth)
+for url in tree.find('urls').findall('url'):
+    get_urls(url.text.strip(), depth)
