433cded1e81e0dd207c751b516920ae861f99552
Павел <paul114burchak@gmail.com>
2018-03-19 02:12:53 +0200

    try id3

--- burchak/url.py
+++ burchak/url.py
@@ -4,9 +4,15 @@ import re
 import xml.etree.ElementTree as ET
 from urllib.parse import urlparse
 
-parsed_uri = urlparse( 'http://stackoverflow.com/questions/1234567/blah-blah-blah-blah' )
-domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-print (domain)
+
+def get_filename(cd):
+    if not cd:
+        return None
+    fname = re.findall('filename=(.+)', cd)
+    if len(fname) == 0:
+        return None
+    return fname[0]
+
 
 def get_urls(url, i):
     page = requests.get(url)
@@ -16,17 +22,22 @@ def get_urls(url, i):
             if re.match(r"https?:\/\/.*", link):
                 get_urls(link, i-1)
             else:
-                parsed_uri = urlparse( url[:len(url)-1] )
+                parsed_uri = urlparse(url[:len(url)-1])
                 domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
                 get_urls(domain+link, i-1)
         else:
-            print(link, "url", url, "\n")
+            if link.endswith('.mp3'):
+                r = requests.get(link, allow_redirects=True)
+                filename = get_filename(r.headers.get('content-disposition'))
+                if filename is not None:
+                    open(filename, 'wb').write(r.content)
+#                    audio = stagger.read_tag(filename)
+#                    print(audio)
     return
 
 
-tree = ET.parse('ulr.xml')
+tree = ET.parse('url.xml')
 root = tree.getroot()
 depth = int(root.find('depth').text)
-print(depth)
 for url in tree.find('urls').findall('url'):
     get_urls(url.text.strip(), depth)
